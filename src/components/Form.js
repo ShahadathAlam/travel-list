047182c235fd635947b044d66941db9bf373b944
Md Shahadath Alam <126391393+ShahadathAlam@users.noreply.github.com>
2024-06-12 16:52:33 +0600

    Add files via upload

---
+++ src/components/Form.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+export default function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // console.log(e);
+    if (!description) return;
+    const newItem = { quantity, description, packed: false, id: Date.now() };
+    // console.log(newItem);
+    onAddItems(newItem);
+    setQuantity(1);
+    setDescription("");
+  }
+  return (
+    // <form className="add-form" onSubmit={e=> handleSubmit(e)}>
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😊 trip?</h3>
+      {/* <select>
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+            </select> */}
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
